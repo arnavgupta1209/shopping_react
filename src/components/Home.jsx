3610b1713a58bc378eec7e7f3cf6ddeb5794fff4
arnavgupta1209 <arnavgupta1209@gmail.com>
2024-02-10 15:24:15 +0530

    made some components

---
+++ src/components/Home.jsx
@@ -0,0 +1,14 @@
+import Navigation from "./Navigation";
+
+
+
+function Home() {
+  return (<>
+    <Navigation />
+    <div>
+      <h1>Home</h1>
+    </div>
+  </>);
+}
+
+export default Home;
---
+++ src/components/Shop.jsx
@@ -0,0 +1,13 @@
+import Navigation from "./Navigation";
+
+
+function Shop() {
+    return (<>
+        <Navigation />
+        <div>
+            <h1>Shop</h1>
+        </div>
+    </>)
+}
+
+export default Shop;
